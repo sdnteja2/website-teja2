099057158b90146f54a7639c411872b81b60a932
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-27 16:49:20 +0700

    Refactor components and update content

--- components/content/Selayang.vue
+++ components/content/Selayang.vue
@@ -1,55 +1,76 @@
+<script setup>
+defineProps({
+  nama: {
+    type: String,
+    default: 'Default title',
+  },
+  sambutan: {
+    type: String,
+    default: 'Default description',
+  },
+  jabatan: {
+    type: String,
+    default: 'Default description',
+  },
+  image: {
+    type: String,
+    default: '/img/man.jpg',
+  },
+})
+</script>
+
 <template>
-  <!-- Testimonials -->
-  <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-    <!-- Grid -->
-    <UContainer class="md:grid md:grid-cols-2 md:gap-10 lg:gap-16 md:items-center">
-      <div class="hidden md:block mb-24 md:mb-0 sm:px-6">
-        <div class="relative">
-          <img class="rounded-xl" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=500&h=600&q=80" alt="Image Description">
-        </div>
-      </div>
-      <!-- End Col -->
-
-      <div>
-        <!-- Blockquote -->
-        <div class="relative">
-          <div class="relative z-10">
-            <p class="text-xs font-semibold text-gray-500 tracking-wide uppercase mb-3 dark:text-gray-200">
-              Featured client
-            </p>
-
-            <p class="text-xl font-medium italic text-gray-800 md:text-2xl md:leading-normal xl:text-3xl xl:leading-normal dark:text-gray-200">
-              To say that switching to Preline has been life-changing is an understatement. My business has tripled and I got my life back.
-            </p>
-          </div>
-
-          <div class="mt-6">
-            <div class="flex items-center">
-              <div class="md:hidden flex-shrink-0">
-                <img class="h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description">
-              </div>
-              <div class="ms-4 md:ms-0">
-                <div class="text-base font-semibold text-gray-800 dark:text-gray-200">
-                  Nicole Grazioso
-                </div>
-                <div class="text-xs text-gray-500 dark:text-gray-400">
-                  Director Payments & Risk | Airbnb
-                </div>
-              </div>
+  <div>
+    <UContainer class="bg-merah patternbg dark:bg-merah-600">
+      <!-- Testimonials -->
+      <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <!-- Grid -->
+        <div class="md:grid md:grid-cols-2 md:gap-10 lg:gap-16 md:items-center">
+          <div class="hidden md:block mb-24 md:mb-0 sm:px-6">
+            <div class="relative">
+              <img class="rounded-xl" :src="image" :alt="nama">
             </div>
           </div>
+          <!-- End Col -->
 
-          <div class="mt-8 lg:mt-14">
-            <a class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-gray-800 text-white hover:bg-gray-900 disabled:opacity-50 disabled:pointer-events-none dark:bg-white dark:text-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-              Read the story
-            </a>
+          <div>
+            <!-- Blockquote -->
+            <blockquote class="relative">
+              <div class="relative z-10">
+                <p class="text-sm font-semibold  tracking-wide uppercase mb-3 text-gray-100 dark:text-gray-900 ">
+                  Selayang Pandang
+                </p>
+
+                <p class="text-xl font-medium   md:text-2xl md:leading-normal xl:text-3xl xl:leading-normal dark:text-gray-300 ">
+                  {{ sambutan }}
+                </p>
+              </div>
+
+              <div class="mt-6">
+                <div class="flex items-center">
+                  <div class="sm:hidden flex-shrink-0">
+                    <img class="rounded-full h-16 w-16" :src="image" :alt="nama">
+                  </div>
+                  <div class="ms-4 md:ms-0">
+                    <div class="text-base font-semibold dark:text-gray-900 text-gray-800 ">
+                      {{ nama }}
+                    </div>
+                    <div class="text-xs  dark:text-gray-300">
+                      {{ jabatan }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </blockquote>
+            <!-- End Blockquote -->
           </div>
+          <!-- End Col -->
         </div>
-      <!-- End Blockquote -->
+        <!-- End Grid -->
       </div>
-    <!-- End Col -->
+      <!-- End Testimonials -->
     </UContainer>
   </div>
-  <!-- End Grid -->
-<!-- End Testimonials -->
 </template>
+
+<style scoped></style>
--- components/content/Visi.vue
+++ components/content/Visi.vue
@@ -1,18 +1,84 @@
+<!-- eslint-disable vue/no-unused-vars -->
+<!-- eslint-disable vue/no-unused-vars -->
 <script setup>
-const items = [{
-  label: 'Tab1',
-  content: 'This is the content shown for Tab1',
-}, {
-  label: 'Tab2',
-  content: 'And, this is the content for Tab2',
-}, {
-  label: 'Tab3',
-  content: 'Finally, this is the content for Tab3',
-}]
+const items = [
+  {
+    label: 'Visi',
+    slot: 'visi',
+  },
+  {
+    label: 'Tujuan',
+    slot: 'tujuan',
+  },
+  {
+    label: 'Misi',
+    slot: 'misi',
+  },
+]
 </script>
 
 <template>
-  <UContainer class="py-20">
-    <UTabs :items="items" :default-index="1" />
+  <UContainer class="py-16 md:py-24 max-w-4xl mx-auto">
+    <UTabs :items="items" size="lg" :default-index="1">
+      <template #misi>
+        <div
+
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          class="px-6 bg-slate-100 shadow dark:bg-slate-900 py-2 rounded-md"
+        >
+          <div class="p-4">
+            <ol class="list-decimal space-y-2">
+              <li>
+                Untuk mencapai visi tersebut melalui misi sebagai berikut:
+              </li>
+              <li>
+                Menciptakan lingkungan sekolah yang bernuansa agamis, bersih dan sehat
+              </li>
+              <li>
+                Meningkatkan kegiatan ibadah melalui kegiatan kultum dan Shalat berjamaah
+              </li>
+              <li>
+                Mengoptimalkan guru profesional dan berdedikasi tinggi sehingga peserta didik mempunyai kompetensi yang baik dan berkarakter
+              </li>
+              <li>
+                Menyelenggarakan pendidikan, pelatihan dan pembinaan berdasarkan bakat, minat dan kreativitas peserta didik melalui ekstrakurikuler
+              </li>
+              <li>
+                Meningkatkan prestasi peserta didik degan mengoptimalkan proses pembelajaran yang efektif dan efisien.
+              </li>
+            </ol>
+          </div>
+        </div>
+      </template>
+      <template #tujuan>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          class="px-6 bg-slate-100 shadow dark:bg-slate-900 py-2 rounded-md"
+        >
+          <div class="p-4">
+            <ol class="list-decimal space-y-2">
+              <li>Terciptanya lingkungan yang agamis, bersih dan sehat</li>
+              <li> Meningkatnya ketakwaan peserta didik terhadap Tuhan Yang Maha Esa</li>
+              <li>  Berkembangnya profesionalisme tenaga pendidik</li>
+              <li>  Terbentuknya pribadi peserta didik yang cerdas, terampil, kreatif, aktif dan berkarakter</li>
+              <li> Meningkatnya jumlah peserta didik yang melanjutkan pendidikan ke jenjang yang lebih tinggi</li>
+            </ol>
+          </div>
+        </div>
+      </template>
+      <template #visi>
+        <div
+          data-aos="fade-up"
+          data-aos-anchor-placement="top-bottom"
+          class="px-6 bg-slate-100 shadow dark:bg-slate-900 py-2 rounded-md"
+        >
+          <div class="p-4">
+            Terwujudnya pribadi yang beriman dan bertakwa kepada Tuhan Tang Maha Esa, berilmu, berkarakter, terampil, kreatif dan berprestasi
+          </div>
+        </div>
+      </template>
+    </UTabs>
   </UContainer>
 </template>
