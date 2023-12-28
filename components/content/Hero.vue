866b9ea5c1d1eb1282be2a1569ae21b76dde6b1e
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-29 03:53:32 +0700

    Update dependencies and font size in Hero component

--- components/content/Hero.vue
+++ components/content/Hero.vue
@@ -19,7 +19,7 @@ defineProps({
   <div class="relative overflow-hidden">
     <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10">
       <div class="max-w-4xl pb-8 text-center mx-auto">
-        <h1 data-aos="fade-up" class="block text-2xl font-extrabold  sm:text-4xl  ">
+        <h1 data-aos="fade-up" class="block text-2xl font-extrabold  md:text-3xl  ">
           SDN Teja 2 <span class="dark:text-gray-200 text-slate-800"> <br>
             {{ title }}
           </span>
