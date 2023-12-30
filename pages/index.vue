625a8ab4cd61b0f288cab0e0c71830c0ee163435
Dinar, Permadi <dinarpermadi07@gmail.com>
2023-12-30 22:23:31 +0700

    Add SEO meta tags and define og:image component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,30 @@
+<script lang="ts" setup>
+const pageTitle = 'SDN Teja 2'
+useSeoMeta({
+  ogImage: '[og:image]',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary',
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'id',
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png',
+    },
+  ],
+})
+defineOgImageComponent('BlogPost', {
+  title: pageTitle,
+})
+</script>
+
 <template>
   <main>
     <ContentDoc path="/" />
